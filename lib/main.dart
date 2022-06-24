6245fa2568b3bb8b0f4003bbd2a0b177a1243a34
Samarjeet09 <singhsamarjeet09@gmail.com>
2022-06-24 16:26:17 +0530

    Step-2

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:notesapp/views/login_view.dart';
+import 'package:notesapp/views/register_view.dart';
 
 import 'firebase_options.dart';
 
@@ -14,6 +15,10 @@ void main() {
         primarySwatch: Colors.pink,
       ),
       home: const HomePage(),
+      routes: {
+        '/login/': (context) => const LoginView(),
+        '/register/': (context) => const RegisterView(),
+      },
     ),
   );
 }
@@ -23,34 +28,32 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("HOME"),
+    return FutureBuilder(
+      //yahan pei we  added a future builder to make sure
+      // that firbase app is initialzed first(as it is a futre) and then the colloum is rendered
+      // toh humnei vo sara code uskei return mei likh diya
+      future: Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
       ),
-      body: FutureBuilder(
-        //yahan pei we  added a future builder to make sure
-        // that firbase app is initialzed first(as it is a futre) and then the colloum is rendered
-        // toh humnei vo sara code uskei return mei likh diya
-        future: Firebase.initializeApp(
-          options: DefaultFirebaseOptions.currentPlatform,
-        ),
-        builder: (context, snapshot) {  
-          //yahan pei loading screen
-          switch (snapshot.connectionState) {
-            case ConnectionState.done:
-              final user = FirebaseAuth.instance.currentUser;
-              if (user?.emailVerified ?? false) {
-                print("You are a verified");
-              } else {
-                print("Verfiy first");
-              }
-              return const Text("Done");
-            default:
-              //basically agar laod na ho ya time le toh yeh display ho
-              return const Text("Loading Please Wait");
-          }
-        },
-      ),
+      builder: (context, snapshot) {
+        //yahan pei loading screen
+        switch (snapshot.connectionState) {
+          case ConnectionState.done:
+            //   final user = FirebaseAuth.instance.currentUser;
+            //   if (user?.emailVerified ?? false) {
+            //     print("You are a verified");
+            //     return const Text("Done");
+            //   } else {
+            //     print(user);
+            //     return const VerifyEmailView();
+            //   }
+            return const LoginView();
+          default:
+            //basically agar laod na ho ya time le toh yeh display ho
+            return const CircularProgressIndicator();
+        }
+      },
     );
   }
 }
+
--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -1,8 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
-import '../firebase_options.dart';
 
 class RegisterView extends StatefulWidget {
   const RegisterView({Key? key}) : super(key: key);
@@ -34,69 +31,54 @@ class _RegisterViewState extends State<RegisterView> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Registeration Page"),
+        title: const Text("Register"),
       ),
-      body: FutureBuilder(
-        //yahan pei we  added a future builder to make sure
-        // that firbase app is initialzed first(as it is a futre) and then the colloum is rendered
-        // toh humnei vo sara code uskei return mei likh diya
-        future: Firebase.initializeApp(
-          options: DefaultFirebaseOptions.currentPlatform,
-        ),
-        builder: (context, snapshot) {
-          //yahan pei loading screen
-          switch (snapshot.connectionState) {
-            case ConnectionState.done:
-              return Column(
-                children: [
-                  TextField(
-                    controller: _email,
-                    decoration:
-                        const InputDecoration(hintText: "Enter your Email"),
-                    // making email field better
-                    autocorrect: false,
-                    enableSuggestions: false,
-                    // we will tell ki email wala keyboard chaiye
-                    keyboardType: TextInputType.emailAddress,
-                  ),
-                  TextField(
-                    controller: _pass,
-                    decoration:
-                        const InputDecoration(hintText: "Enter your Password"),
-                    // now to make it a better password field
-                    obscureText: true,
-                    autocorrect: false,
-                    enableSuggestions: false,
-                  ),
-                  TextButton(
-                    onPressed: () async {
-                      final email = _email.text;
-                      final pass = _pass.text;
-                      // initialzing the firebase--> ek package aur dala  'firebase_options.dart';
-                      try {
-                        final userCredential = await FirebaseAuth.instance
-                            .createUserWithEmailAndPassword(
-                                email: email, password: pass);
-                        print(userCredential);
-                      } on FirebaseAuthException catch (e) {
-                        if (e.code == "weak-password") {
-                          print("weak password");
-                        } else if (e.code == "email-already-in-use") {
-                          print("Email already in use");
-                        } else if (e.code == 'invalid-email') {
-                          print("invalid email");
-                        }
-                      }
-                    },
-                    child: const Text("Register"),
-                  ),
-                ],
-              );
-            default:
-              //basically agar laod na ho ya time le toh yeh display ho
-              return const Text("Loading Please Wait");
-          }
-        },
+      body: Column(
+        children: [
+          TextField(
+            controller: _email,
+            decoration: const InputDecoration(hintText: "Enter your Email"),
+            // making email field better
+            autocorrect: false,
+            enableSuggestions: false,
+            // we will tell ki email wala keyboard chaiye
+            keyboardType: TextInputType.emailAddress,
+          ),
+          TextField(
+            controller: _pass,
+            decoration: const InputDecoration(hintText: "Enter your Password"),
+            // now to make it a better password field
+            obscureText: true,
+            autocorrect: false,
+            enableSuggestions: false,
+          ),
+          TextButton(
+            onPressed: () async {
+              final email = _email.text;
+              final pass = _pass.text;
+              // initialzing the firebase--> ek package aur dala  'firebase_options.dart';
+              try {
+                final userCredential = await FirebaseAuth.instance
+                    .createUserWithEmailAndPassword(
+                        email: email, password: pass);
+                print(userCredential);
+              } on FirebaseAuthException catch (e) {
+                if (e.code == "weak-password") {
+                  print("weak password");
+                } else if (e.code == "email-already-in-use") {
+                  print("Email already in use");
+                } else if (e.code == 'invalid-email') {
+                  print("invalid email");
+                }
+              }
+            },
+            child: const Text("Register"),
+          ),
+          TextButton(
+              onPressed: () => Navigator.of(context)
+                  .pushNamedAndRemoveUntil('/login/', (route) => false),
+              child: const Text('Login Instead'))
+        ],
       ),
     );
   }
